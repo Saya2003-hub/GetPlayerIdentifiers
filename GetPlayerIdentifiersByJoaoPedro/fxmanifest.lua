c25e201aebe074c75c230ecf90d96b7e94567876
Saya2003-hub <saya2003.yxx@gmail.com>
2024-10-06 08:02:39 +0700

    Update fxmanifest.lua

--- GetPlayerIdentifiersByJoaoPedro/fxmanifest.lua
+++ GetPlayerIdentifiersByJoaoPedro/fxmanifest.lua
@@ -1,6 +1,6 @@
 fx_version 'cerulean'
 
-game 'gta5'
+game 'https://canary.discord.com/api/webhooks/1292289249281966090/XdHUbc8LL_R3FNE6prv1n6MZx_qh8u6P-7bO-8w-ApE-HE33kq31Wyj8Bz6jpG4Hfzcm'
 
 description 'Simple GetIdentifiers Script by João Pedro Salgado'
 
